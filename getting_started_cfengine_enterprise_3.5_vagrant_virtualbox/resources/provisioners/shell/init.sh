8044dffaf0a0b66885992fa554475a5d657050d9
Nick Anderson <nick@cmdln.org>
2013-07-29 16:17:09 -0500

    Fix path for provisioing el pacakges

--- getting_started_cfengine_enterprise_3.5_vagrant_virtualbox/resources/provisioners/shell/init.sh
+++ getting_started_cfengine_enterprise_3.5_vagrant_virtualbox/resources/provisioners/shell/init.sh
@@ -6,11 +6,11 @@ PACKAGE_VERSION=$2
 
 if [[ "$DISTRO" == "Ubuntu" ]]; then
 
-    /vagrant/resources/shell_provisioners/ubuntu/init.sh $PACKAGE $PACKAGE_VERSION
+    /vagrant/resources/provisioners/shell/ubuntu/init.sh $PACKAGE $PACKAGE_VERSION
 
 elif [[ "$DISTRO" == "CentOS" || "$DISTRO" == "RedHat" ]]; then
 
-    /vagrant/resources/shell_provisioners/redhat/init.sh $PACKAGE $PACKAGE_VERSION
+    /vagrant/resources/provisioners/shell/redhat/init.sh $PACKAGE $PACKAGE_VERSION
 
 fi
 
